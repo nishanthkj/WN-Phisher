c239e9217a0d1bf2d5c4a62b8d21a3245c8e8718
nishanthkj <nishanthkj9@gmail.com>
2024-05-29 19:50:54 +0530

    Commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
